733bc05134f46a70556ff897f780af3edff249f6
jefferson <jefferson@CARRILLO_JAC>
2016-08-24 11:03:50 -0500

    CUARTA MODIFICACION

---
+++ Ingenieria_Software_Tarea/src/ingenieria_software_tarea/Calculardora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ingenieria_software_tarea;
+
+/**
+ *
+ * @author jefferson
+ */
+public class Calculardora {
+    
+    public static int suma (int a, int b){
+        int c=a+b;
+        return c;
+        
+    }
+}
